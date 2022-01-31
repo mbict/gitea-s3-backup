45a526d37a428b24c89922fb2111c446e2a12021
Michael Boke <michael@mbict.nl>
2022-01-31 08:14:02 +0100

    reverted back sh run as

--- backup.sh
+++ backup.sh
@@ -5,5 +5,5 @@ chmod 775 /backup
 
 DUMP_FILENAME=/backup/gitea-dump-$(date +"%Y-%m-%d").zip
 
-su -c "/usr/local/bin/gitea dump --file ${DUMP_FILENAME}" -m  git
+/usr/local/bin/gitea dump --file ${DUMP_FILENAME}
 /usr/local/bin/s3backup ${S3_BUCKET} ${S3_MAP} ${DUMP_FILENAME} ${MAXCOPIES}
