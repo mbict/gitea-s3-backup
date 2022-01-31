20f90112c8bb17192bbc515bf5ee18c04215b90d
Michael Boke <michael@mbict.nl>
2022-01-31 08:05:40 +0100

    Updated path and act as user for gitea dump

--- backup.sh
+++ backup.sh
@@ -2,10 +2,8 @@
 
 mkdir /backup
 chmod 775 /backup
-su git
 
-DUMP_FILENAME=/gitea-dump-$(date +"%Y-%m-%d").zip
+DUMP_FILENAME=/backup/gitea-dump-$(date +"%Y-%m-%d").zip
 
-echo ${DUMP_FILENAME}
-/usr/local/bin/gitea dump --file ${DUMP_FILENAME}
+su -c "/usr/local/bin/gitea dump --file ${DUMP_FILENAME}" -m  git
 /usr/local/bin/s3backup ${S3_BUCKET} ${S3_MAP} ${DUMP_FILENAME} ${MAXCOPIES}
