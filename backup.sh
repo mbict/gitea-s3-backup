6f46ff4f2cd86815c54e68a525fef2cd43bfa886
Michael Boke <michael@mbict.nl>
2022-01-31 10:21:51 +0100

    added more user permissions to commands

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 set -e
 
 mkdir /backup
-chmod 775 /backup
+chown git /backup
 
 DUMP_FILENAME="/backup/gitea-dump-$(date +"%Y-%m-%d").zip"
 
@@ -11,7 +11,7 @@ echo "running gitea dump as user git"
 su - git -c "/usr/local/bin/gitea dump --file ${DUMP_FILENAME}"
 
 echo "running s3backup"
-/usr/local/bin/s3backup ${S3_BUCKET} ${S3_MAP} ${DUMP_FILENAME} ${MAXCOPIES}
+su - git -c "/usr/local/bin/s3backup ${S3_BUCKET} ${S3_MAP} ${DUMP_FILENAME} ${MAXCOPIES}"
 
 echo "cleaning up"
 rm -f ${DUMP_FILENAME}
