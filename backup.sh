54bf061098916f5770611a035e0a7ddc6623017c
Michael Boke <michael@mbict.nl>
2022-01-31 08:30:02 +0100

    altered sh file

--- backup.sh
+++ backup.sh
@@ -1,9 +1,19 @@
-#! /bin/sh
+#!/bin/sh
+
+set -e
 
 mkdir /backup
 chmod 775 /backup
 
-DUMP_FILENAME=/backup/gitea-dump-$(date +"%Y-%m-%d").zip
+DUMP_FILENAME="/backup/gitea-dump-$(date +"%Y-%m-%d").zip"
 
-/usr/local/bin/gitea dump --file ${DUMP_FILENAME}
+echo "running gitea dump as user git"
+su - git -c "/usr/local/bin/gitea dump --file ${DUMP_FILENAME}"
+
+echo "running s3backup"
 /usr/local/bin/s3backup ${S3_BUCKET} ${S3_MAP} ${DUMP_FILENAME} ${MAXCOPIES}
+
+echo "cleaning up"
+rm -f ${DUMP_FILENAME}
+
+echo "backup complete"
