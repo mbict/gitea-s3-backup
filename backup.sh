c696f353a780085828d5b3d641e0b10aea8e9900
Michael Boke <michael@mbict.nl>
2022-01-31 11:40:57 +0100

    removed sudo

--- backup.sh
+++ backup.sh
@@ -5,10 +5,10 @@ set -e
 DUMP_FILENAME="/backup/gitea-dump-$(date +"%Y-%m-%d").zip"
 
 echo "running gitea dump as user git"
-sudo -Eu git /usr/local/bin/gitea dump --file ${DUMP_FILENAME}
+/usr/local/bin/gitea dump --file ${DUMP_FILENAME}
 
 echo "running s3backup"
-sudo -Eu git /usr/local/bin/s3backup ${S3_BUCKET} ${S3_MAP} ${DUMP_FILENAME} ${MAXCOPIES}
+/usr/local/bin/s3backup ${S3_BUCKET} ${S3_MAP} ${DUMP_FILENAME} ${MAXCOPIES}
 
 echo "cleaning up"
 rm -f ${DUMP_FILENAME}
